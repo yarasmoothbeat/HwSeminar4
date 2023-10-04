db83015b931ac58baaa841789549679e6866f486
master <yarasmoothbeat@gmail.com>
2023-10-04 12:08:34 +0500

    29

---
+++ Задача29/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write( array[i] + " " );
+    }
+}
+
+System.Console.WriteLine("Input array size: " );
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value: " );
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value: " );
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+PrintArray(myArray);
