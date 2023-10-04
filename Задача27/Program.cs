2ece1841d0b5b1d46c506fa3109c0df02c5b0d9e
master <yarasmoothbeat@gmail.com>
2023-10-04 12:06:50 +0500

    27

---
+++ Задача27/Program.cs
@@ -0,0 +1,21 @@
+﻿int promt(string message)
+{
+    Console.Write("Введите число");
+    string.readInput = Console.ReadLIne();
+    int result = int.Parse(readinput);
+    return result;
+}
+
+int amount(int num)
+{
+    int res = 0;
+    while (num > 0)
+    {
+        res += num % 10;
+        num = num / 10;
+    }
+    return res;
+}
+
+int number = promt;
+Console.WriteLine (amount(number));
